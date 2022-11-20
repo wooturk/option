f2e7e019014009278791357c4c35f47d2391a8a1
tulparstudyo <teknovipyazilim@gmail.com>
2022-11-20 22:10:19 +0300

    first

---
+++ src/OptionServiceProvider.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Wooturk;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Route;
+class OptionServiceProvider extends ServiceProvider
+{
+	/**
+	 * Register services.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Bootstrap services.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		Route::get('/option', [OptionController::class, 'index'])->name('option-index');
+		Route::get('/options', [OptionController::class, 'list'])->name('option-list');
+		Route::get('/option/{id}', [OptionController::class, 'get'])->name('option-get');
+		Route::get('/option/{id}/values', [OptionController::class, 'list'])->name('option-values-list');
+		Route::get('/option/{id}/value/{value_id}', [OptionController::class, 'get'])->name('option-value-get');
+		Route::group(['middleware' => ['auth:sanctum','wooturk.gateway']], function(){
+			Route::post('/option', [OptionController::class, 'post'])->name('option-create');
+			Route::put('/option/{id}', [OptionController::class, 'put'])->name('option-update');
+			Route::delete('/option/{id}', [OptionController::class, 'delete'])->name('option-delete');
+		});
+	}
+}
