2fade3f44ba1d30db3eef064be24bd18e0345ca0
tulparstudyo <teknovipyazilim@gmail.com>
2022-11-20 22:24:42 +0300

    Update OptionServiceProvider.php

--- src/OptionServiceProvider.php
+++ src/OptionServiceProvider.php
@@ -26,12 +26,14 @@ class OptionServiceProvider extends ServiceProvider
 		Route::get('/option', [OptionController::class, 'index'])->name('option-index');
 		Route::get('/options', [OptionController::class, 'list'])->name('option-list');
 		Route::get('/option/{id}', [OptionController::class, 'get'])->name('option-get');
-		Route::get('/option/{id}/values', [OptionController::class, 'list'])->name('option-values-list');
-		Route::get('/option/{id}/value/{value_id}', [OptionController::class, 'get'])->name('option-value-get');
+		Route::get('/option/{id}/values', [OptionController::class, 'list_value'])->name('option-values-list');
+		Route::get('/option/{id}/value/{value_id}', [OptionController::class, 'get_value'])->name('option-value-get');
 		Route::group(['middleware' => ['auth:sanctum','wooturk.gateway']], function(){
+			Route::post('/option', [OptionController::class, 'post'])->name('option-create');
 			Route::post('/option', [OptionController::class, 'post'])->name('option-create');
 			Route::put('/option/{id}', [OptionController::class, 'put'])->name('option-update');
 			Route::delete('/option/{id}', [OptionController::class, 'delete'])->name('option-delete');
+			Route::post('/option/{id}/value', [OptionController::class, 'post_value'])->name('option-value-create');
 		});
 	}
 }
